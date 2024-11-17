ecaab52ed678b76356e8629d28db1b5159b5386e
Jeffrey Wong <jvwong@gmail.com>
2024-11-16 23:16:02 -0500

    End Chapter 11

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from 'next';
+import NextConfig from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    ppr: 'incremental',
-  },
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
 };
 
 export default nextConfig;
